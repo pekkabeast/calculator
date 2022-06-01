5445ae6d546f56981a334b691a1299a241d4e1ec
Victor <vctrchn@hotmail.ca>
2022-05-31 20:47:41 -0400

    adding buttons with style

--- script.js
+++ script.js
@@ -1,35 +1,33 @@
-function add(a,b){
+function add(a, b) {
     return a + b;
 
 }
 
-function subtract(a,b){
-    return a-b;
+function subtract(a, b) {
+    return a - b;
 
 }
 
-function multiply(a,b){
-    return a*b;
+function multiply(a, b) {
+    return a * b;
 
 }
 
-function divide(a,b){
-    return a/b;
+function divide(a, b) {
+    return a / b;
 
 }
 
-function operate(a,b,operator){
-    if(operator == '+'){
-        add(a,b);
-    }
-    else if (operator == '-'){
-        subtract(a,b);
-    }
-    else if (operator == '*'){
-        multiply(a,b);
-    }
-    else if (operator == '/'){
-        divide(a,b);
+function operate(a, b, operator) {
+    if (operator == 'add') {
+        add(a, b);
+        console.log(add(a, b));
+    } else if (operator == 'sub') {
+        subtract(a, b);
+    } else if (operator == 'mult') {
+        multiply(a, b);
+    } else if (operator == 'div') {
+        divide(a, b);
     }
 
 }
